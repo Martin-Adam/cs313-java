7e2c4fe7d840cc52cb11167a7fc2b8c7daef50f6
Martin-Adam <copperdog08@gmail.com>
2015-06-20 23:12:31 -0600

    Create Login.java

---
+++ src/main/java/com/mycompany/realproject/Login.java
@@ -0,0 +1,53 @@
+package thread;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+
+        if (username == null || username.equals("")) {
+            response.sendRedirect("./invalid.jsp");
+        }
+
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("username") == null) {
+            session.setAttribute("username", username);
+        }
+
+        response.sendRedirect("./newPost.jsp");          
+        
+        
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
